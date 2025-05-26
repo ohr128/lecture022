a6e7de8f408611b8c09bc7a8ecf156bd34b9b221
ohr <ohr_2018_naver.com>
2025-05-26 11:42:31 +0900

    feat: 두번째 커밋

---
+++ src/main/java/com/example/lecture022/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.lecture022;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("hello")
+	public String helloWorld() {
+		return ("Hello World");
+
+	}
+
+}
